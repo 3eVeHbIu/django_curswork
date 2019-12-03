a4d961f8592a7156fc5b99366ec5d9a165344fb8
Sergey Arefiev <sergei.arefev2000@yandex.ru>
2019-12-03 17:42:43 +0300

    made validation + some marks

--- NewsFeed/forms.py
+++ NewsFeed/forms.py
@@ -1,20 +1,35 @@
 from django import forms
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+from django.core import validators
 
 
 class UserForm(forms.ModelForm):
     username = forms.CharField(label='Имя пользователя',
+                               min_length=4,
                                widget=forms.widgets.TextInput(attrs={'placeholder': 'Ivan', 'class': 'form-control'}))
     email = forms.EmailField(label='Email',
                              widget=forms.widgets.EmailInput(attrs={'placeholder': 'ivan@mail.com',
                                                                     'class': 'form-control'}))
     first_name = forms.CharField(label='Имя',
+                                 min_length=2,
+                                 validators=[validators.RegexValidator(
+                                     regex='^([A-zА-я])+$')],
+                                 error_messages={
+                                     'invalid': 'Имя может содержать только Латиницу или Кириллицу'},
                                  widget=forms.widgets.TextInput(attrs={'placeholder': 'Иван', 'class': 'form-control'}),)
     last_name = forms.CharField(label='Фамилия',
+                                min_length=2,
+                                validators=[validators.RegexValidator(
+                                    regex='^([A-zА-я])+$')],
+                                error_messages={
+                                    'invalid': 'Фамилия может содержать только Латиницу или Кириллицу'},
                                 widget=forms.widgets.TextInput(attrs={'placeholder': 'Иванов', 'class': 'form-control'}))
     password1 = forms.CharField(label='Пароль',
+                                min_length=6,
                                 widget=forms.widgets.PasswordInput(attrs={'class': 'form-control'}))
     password2 = forms.CharField(label='Повторить пароль',
+                                min_length=6,
                                 widget=forms.widgets.PasswordInput(attrs={'class': 'form-control'}))
 
     class Meta:
@@ -22,11 +37,16 @@ class UserForm(forms.ModelForm):
         fields = ('email', 'username', 'password1',
                   'password2', 'first_name', 'last_name')
 
-    def clean_password2(self):
-        cd = self.cleaned_data
-        if cd['password1'] != cd['password2']:
-            raise forms.ValidationError('Пароли не совпадают.')
-        return cd['password2']
+    def clean(self):
+        super().clean()
+        errors = {}
+        if self.cleaned_data['password1'] != self.cleaned_data['password2']:
+            errors['password1'] = ValidationError('Пароли не совпадают')
+        if errors:
+            raise ValidationError(errors)
 
-    def clean_fild(self):
-        pass
+# Написать функцию которая вещает на поля классы is-valid/is-invalid
+
+
+class NewsForm(forms.ModelForm):
+    pass
--- NewsFeed/views.py
+++ NewsFeed/views.py
@@ -8,20 +8,21 @@ def index(request):
 
 
 def registration(request):
-    # if user auntificated
-
-    if request.method == 'POST':
-        form = UserForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            # return redirect('login')
-            login(request, user)
-            return redirect('index')
-        else:
-            errors = UserForm.errors
-            context = {'form': form, 'errors': errors}
-            return render(request, 'registration/registration.html', context)
+    if request.user.is_authenticated:
+        return redirect('index')
     else:
-        form = UserForm()
-        context = {'form': form}
-        return render(request, 'registration/registration.html', context)
+        if request.method == 'POST':
+            form = UserForm(request.POST)
+            if form.is_valid():
+                user = form.save()
+                # return redirect('login') #Доделать активацию по почте
+                login(request, user)
+                return redirect('index')
+            else:
+                errors = UserForm.errors
+                context = {'form': form, 'errors': errors}
+                return render(request, 'registration/registration.html', context)
+        else:
+            form = UserForm()
+            context = {'form': form}
+            return render(request, 'registration/registration.html', context)
